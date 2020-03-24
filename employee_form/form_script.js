4c947662e5f18e7cf5bd007fdb9e2905efe13d15
sagor1155 <sagor.eee.cuet@gmail.com>
2020-03-24 20:32:58 +0600

    simple employee form designed using bootstrap & jquery

---
+++ employee_form/form_script.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function(){
+
+    $("#submit_button").click(function(event){
+        event.preventDefault();
+        createTableBody(
+            $("#employeeId").val(), 
+            $("#employeeName").val(), 
+            $('input[name=radio_status]:checked').val(), 
+            getJobDescription(), 
+            $("#district_select").val());
+    });
+
+    function createTableBody(id, name, status, jd, district) {
+        var tabletd = undefined; 
+        var tableData = undefined;
+        var tableDataArray = [id, name, status, jd, district];
+    
+        var table = $("#table01");
+        var tableBody = '<tbody>';
+        var tableRow  = '<tr>';
+    
+        for(var index=0; index<tableDataArray.length; index++){
+           tabletd = '<td>';
+           tableData = tableDataArray[index];
+           tabletd  += tableData + '</td>';
+           tableRow += tabletd;
+        }
+    
+        tableRow += '</tr>';
+        tableBody = tableRow + '</tbody>';
+        table.html(table.html() + tableBody);
+    }
+
+    function getJobDescription(){
+        var jobDescription    = "";
+        var jobDescriptionUx  = $('input[name=UX]:checked').val();
+        var jobDescriptionDev = $('input[name=DEV]:checked').val();
+        var jobDescriptionDb  = $('input[name=DB]:checked').val();
+    
+        if(jobDescriptionUx){
+            jobDescription += " UX";
+        }
+        if(jobDescriptionDev){
+            jobDescription += " DEV";
+        }
+        if(jobDescriptionDb){
+            jobDescription += " DB";
+        }
+        return jobDescription;
+    }
+});
